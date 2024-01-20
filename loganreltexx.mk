a21e7a6c69c5d8cb98ed576ce2b430627c61af6a
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2024-01-20 17:31:18 +0100

    bruh

--- loganreltexx.mk
+++ loganreltexx.mk
@@ -22,9 +22,6 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/loganreltexx/overlay
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
-    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
